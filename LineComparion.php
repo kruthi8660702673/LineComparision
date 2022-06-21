732e415586fdb7d8d2a65b750dce1144fc51f72f
Kruthika <kruthikagnayaka05@gmail.com>
2022-06-21 23:32:28 +0530

    classes and objects

--- LineComparion.php
+++ LineComparion.php
@@ -1,27 +1,43 @@
 <?php
-echo "..... Welcome to Line Comparison Computation Program .......\n ";
+class LineComparision{
+    function welcomeMsg(){
+        echo "..... Welcome to Line Comparison Computation Program .......\n ";
+    }
+    public $x1, $x2, $y1, $y2 ;
 
-$x1 =readline('Enter the value of x1: ');
-$x2 =readline('Enter the value of x2: ');
-$y1 =readline('Enter the value of y1: ');
-$y2 =readline('Enter the value of y2: ');
+function readValue (){
+   $this-> x1 =readline('Enter the value of x1: ');
+   $this-> x2 =readline('Enter the value of x2: ');
+   $this-> y1 =readline('Enter the value of y1: ');
+   $this-> y2 =readline('Enter the value of y2: ');
+}
+function lineComp(){
+    $lengthOfline = sqrt(pow(($this->x2-$this->x1),2)+ pow(($this->y2-$this->y1),2));
+    return $lengthOfline;
+}
 
-$x3 =readline('Enter the value of x3: ');
-$x4 =readline('Enter the value of x4: ');
-$y3 =readline('Enter the value of y3: ');
-$y4 =readline('Enter the value of y4: ');
-function lineComp($x1,$x2,$y1,$y2,$x3,$x4,$y3,$y4){
-$LengthOfLine1 = sqrt(pow(($x2-$x1),2)+ pow(($y2-$y1),2));
-echo "The length of the line is ". $LengthOfLine1 . "\n";
-$LengthOfLine2 = sqrt(pow(($x4-$x3),2)+ pow(($y4-$y3),2));
-echo "The length of the line is ". $LengthOfLine2 . "\n";
-if($LengthOfLine1 == $LengthOfLine2){
-    echo "Both lines are equal \n";
-}else if ($LengthOfLine1 > $LengthOfLine2){
-    echo "Line1 is greater than Line2 \n ";  
-}else {
-    echo "Line2 is greater than Line2";
+public function comparingLength($lengthOfline1, $lengthOfline2){
+    if($lengthOfline1 == $lengthOfline2){
+        echo "Both lines are equal \n";
+    }else if ($lengthOfline1 > $lengthOfline2){
+        echo "Line1 is greater than Line2 \n ";  
+    }else {
+        echo "Line2 is greater than Line2";
+    }
 }
+
 }
-lineComp($x1,$x2,$y1,$y2,$x3,$x4,$y3,$y4);
-?>
+
+$line =new LineComparision();
+$line->welcomeMsg();
+
+$lengthOfline1 =$line->readValue();
+echo "input values for line 1\n";
+$lengthOfline1 = $line->lineComp();   
+echo "Length of the line $lengthOfline1 \n";
+
+$lengthOfline2 =$line->readValue();
+echo "input values for line Line2\n";
+$lengthOfline2 = $line->lineComp();   
+echo "Length of the line $lengthOfline2 \n";
+$line->comparingLength($lengthOfline1, $lengthOfline2);
